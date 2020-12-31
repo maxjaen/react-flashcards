3044a8699ecfca6cada91a653577d2993a7a57e2
Max Jänicke <max.jaenicke@gmx.de>
2020-12-31 17:26:28 +0100

    deleted example code

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import flashcardsReducer from '../features/flashcards/flashcardsSlice';
 
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     flashcards : flashcardsReducer
   },
 });
